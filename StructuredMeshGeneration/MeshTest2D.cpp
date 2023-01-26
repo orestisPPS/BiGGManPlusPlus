30efeb9a800b55a11fa3540096a8e159b2141088
orestis.PPS <orestis.pps@gmail.com>
2023-01-26 03:11:48 +0200

    coordinates are assignes. some more testing for 2d until majol millstone

--- StructuredMeshGeneration/MeshTest2D.cpp
+++ StructuredMeshGeneration/MeshTest2D.cpp
@@ -9,10 +9,10 @@ namespace StructuredMeshGenerator {
     
         MeshTest2D::MeshTest2D() {
             map<Direction, unsigned> numberOfNodes;
-            numberOfNodes[Direction::One] = 3;
-            numberOfNodes[Direction::Two] = 3;
+            numberOfNodes[Direction::One] = 5;
+            numberOfNodes[Direction::Two] = 5;
             numberOfNodes[Direction::Three] = 0;    
-            MeshSpecs specs = MeshSpecs(numberOfNodes, 0.5, 1, 2, 0, 0);
+            MeshSpecs specs = MeshSpecs(numberOfNodes, 1, 1, 0, 0, 0);
             auto space = (PositioningInSpace::OneTwo_plane);
             auto mesh = MeshPreProcessor(specs, reinterpret_cast<PhysicalSpaceEntity &>(space)).mesh;
             
@@ -22,7 +22,14 @@ namespace StructuredMeshGenerator {
                     cout << "Global id: " << *(mesh->node(i,j)->id.global) << endl;
                     cout << "Boundary id: " << *(mesh->node(i,j)->id.boundary) << endl;
                     cout << "Internal id: " << *(mesh->node(i,j)->id.internal) << endl;
-                    cout << "Template Coordinates: " << mesh->node(i,j)->positionVector().at(Template)<<endl;
+                    
+                    cout << "Natural Coord: (" << (mesh->node(i,j)->positionVector(Natural)[0])  << ", "
+                                               << (mesh->node(i,j)->positionVector(Natural)[1]) << ")" <<  endl;
+                    cout << "Parametric Coord: (" << (mesh->node(i,j)->positionVector(Parametric)[0]) << ", "
+                                                  << (mesh->node(i,j)->positionVector(Parametric)[1]) << ")" <<  endl;
+                    cout << "Template Coord: (" << (mesh->node(i,j)->positionVector(Template)[0]) << ", "
+                                                << (mesh->node(i,j)->positionVector(Template)[1]) << ")" <<  endl;
+                    
                     cout << "-------------" << endl;
                     
                 }
